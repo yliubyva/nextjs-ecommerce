e164d2a2017fe0cb9c87e9daa36ba865a9f6480b
yliubyva <yliubyva@gmail.com>
2025-05-15 14:44:00 +0300

    chore: integrate svgr, embla-carousel, clsx and configure public path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.(".svg"),
+    );
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: fileLoaderRule.issuer,
+      use: ["@svgr/webpack"],
+    });
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
 export default nextConfig;
