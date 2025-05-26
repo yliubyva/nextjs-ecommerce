d81786ef1a317e66af1023a089455c189d4b9835
yliubyva <yliubyva@gmail.com>
2025-05-26 18:54:18 +0300

    feat: create Stars component for rating display

---
+++ src/components/ui-kit/Stars.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+type Props = {
+  rating: number;
+  showNum: boolean;
+};
+
+export const Stars: React.FC<Props> = ({ rating, showNum }) => {
+  if (rating > 5) {
+    rating = 5;
+  }
+
+  const fullStars = Math.floor(rating);
+  const hasHalfStar = rating % 1 !== 0;
+
+  return (
+    <div className="flex gap-[11px]">
+      <div className="flex gap-[4px]">
+        {Array.from({ length: fullStars }).map((_, i) => (
+          <Image
+            key={`full-${i}`}
+            src="/icons/star-icon.svg"
+            alt="star icon"
+            width={16}
+            height={16}
+            className="xl:h-[18px] xl:w-[18px]"
+          />
+        ))}
+        {hasHalfStar && (
+          <Image
+            key="half"
+            src="/icons/half-star-icon.svg"
+            alt="star icon"
+            width={8}
+            height={16}
+            className="md:h-[18px] md:w-[9px]"
+          />
+        )}
+      </div>
+      {showNum && (
+        <p>
+          {rating}/<span className="text-gray-500">5</span>
+        </p>
+      )}
+    </div>
+  );
+};
