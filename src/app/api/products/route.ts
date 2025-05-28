60040db8f92d375a544696e76aafee7bc034ca82
yliubyva <yliubyva@gmail.com>
2025-05-28 20:19:17 +0300

    feat: add product images, mock data and fake products API endpoint

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,6 @@
+import { products } from "@/data/products";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json(products);
+}
---
+++ src/types/product.ts
@@ -0,0 +1,33 @@
+type Color = {
+  name: string;
+  hex: string;
+  images: string[];
+};
+
+type Review = {
+  author: string;
+  date: string;
+  rating: number;
+  text?: string;
+};
+
+export type Product = {
+  id: string;
+  title: string;
+  category: "Men" | "Women";
+  type: "T-shirt" | "Shorts" | "Shirts" | "Hoodie" | "Jeans";
+  brand: "Versache" | "Zara" | "Gucci" | "Prade" | "Calvin Klein";
+  price: number;
+  discount: number | null;
+  rating: number;
+  colors: Color[];
+  sizes: Array<"Small" | "Medium" | "Large" | "X-Large">;
+  description: string;
+  popularity: number;
+  reviews?: Review[];
+  arrivalDate: Date | string;
+  inStock: boolean;
+  tags?: string[];
+  dressStyle: "Casual" | "Formal" | "Party" | "Gym";
+};
+
