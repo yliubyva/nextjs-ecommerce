ca3c200d5268bdedc3ac8296f1885c6c27c710d0
yliubyva <yliubyva@gmail.com>
2025-07-16 21:08:48 +0300

    style: update QuantityCounter styles based on isCart prop

--- src/components/ui-kit/QuantityCounter.tsx
+++ src/components/ui-kit/QuantityCounter.tsx
@@ -1,14 +1,17 @@
 import Minus from "@public/icons/minus.svg";
 import Plus from "@public/icons/plus.svg";
+import clsx from "clsx";
 
 type Props = {
   counter: number;
   onCounterChange: (counter: number) => void;
+  isCart: boolean;
 };
 
 export const QuantityCounter: React.FC<Props> = ({
   counter,
   onCounterChange,
+  isCart,
 }) => {
   const handleIncreaseCount = () => {
     if (counter === 10) return;
@@ -19,14 +22,27 @@ export const QuantityCounter: React.FC<Props> = ({
     onCounterChange(counter - 1);
   };
   return (
-    <div className="grid w-full max-w-[170px] grid-cols-[20px_20px_20px] grid-rows-1 items-center justify-between rounded-full bg-[#F0F0F0] px-[16px] py-[12px] md:px-[20px] md:py-[14px]">
-      <button className="cursor-pointer" onClick={handlerDecreaseCount}>
+    <div
+      className={clsx(
+        "grid w-full grid-cols-[20px_20px_20px] grid-rows-1 items-center justify-between rounded-full bg-[#F0F0F0] px-[16px] md:px-[20px]",
+        isCart
+          ? "max-w-[105px] py-[6px] md:max-w-[124px] md:py-[12px]"
+          : "max-w-[170px] py-[12px] md:py-[14px]",
+      )}
+    >
+      <button
+        className="h-[20px] w-[20px] cursor-pointer"
+        onClick={handlerDecreaseCount}
+      >
         <Minus width={16} />
       </button>
       <p className="justify-self-center text-sm font-normal xl:text-base">
         {counter}
       </p>
-      <button className="cursor-pointer" onClick={handleIncreaseCount}>
+      <button
+        className="h-[20px] w-[20px] cursor-pointer"
+        onClick={handleIncreaseCount}
+      >
         <Plus width={16} />
       </button>
     </div>
