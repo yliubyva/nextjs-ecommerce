3e006f7c20ca24088103f6660e82f9c8347eb0cd
yliubyva <yliubyva@gmail.com>
2025-09-02 00:50:04 +0300

    refactor: remove async from cart page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
 import { SearchClient } from "@/features/productCatalog/components/SearchClient";
 import { Container } from "@/shared/ui/atoms/Container";
 
-export default async function SearchResultsPage() {
+export default function SearchResultsPage() {
   return (
     <Container>
       <SearchClient />
